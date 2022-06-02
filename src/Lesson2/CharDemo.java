0fb7166318cb7393bb64e66eddfe831d10778598
Ld-Ld-Ld <ldld@inbox.ru>
2022-06-02 22:37:37 +0300

    my first commit

---
+++ src/Lesson2/CharDemo.java
@@ -0,0 +1,14 @@
+package Lesson2;
+
+public class CharDemo {
+    public static void main (String[] args){
+
+        char ch;
+        ch = 'X';
+        System.out.println("ch содержит " + ch);
+
+        ch++;
+        System.out.println("теперь ch содержит " + ch);
+
+    }
+}
