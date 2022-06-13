79b94ed3518573334686ef328b74c9d2db85bce4
Ld-Ld-Ld <ldld@inbox.ru>
2022-06-13 16:39:37 +0300

    my first commit

---
+++ src/Homework/SortNum.java
@@ -0,0 +1,34 @@
+package Homework;
+
+import java.util.Scanner;
+
+public class SortNum {
+    public static void main(String[] args) {
+
+            Scanner input1 = new Scanner(System.in);
+            System.out.println("Введите первое целое число:");
+            int x = input1.nextInt();
+
+            Scanner input2 = new Scanner(System.in);
+            System.out.println("Введите второе целое число:");
+            int y = input2.nextInt();
+
+            Scanner input3 = new Scanner(System.in);
+            System.out.println("Введите третье целое число:");
+            int z = input3.nextInt();
+
+            Scanner input4 = new Scanner(System.in);
+            System.out.println("Введите четвертое целое число:");
+            int d = input4.nextInt();
+
+            Scanner input5 = new Scanner(System.in);
+            System.out.println("Введите пятое целое число:");
+            int t = input5.nextInt();
+
+
+
+
+
+        }
+    }
+
